36c1c2a2108fd057e6ffd3f0c9cd3c3cc86838b2
Muhammad Qaiser <osama@osama.gateway.sasktel.net>
2021-04-03 14:05:06 -0600

    Decided to omit the feature of adding sound

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ const App = () => {
       userName={localStorage.getItem('username')}
       userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      //onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
 };
